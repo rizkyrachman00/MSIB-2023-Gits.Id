498794837fda1d6d8cc560e5a903f3b137d1bd9d
rizkyrachman00 <97326712+rizkyrachman00@users.noreply.github.com>
2023-08-23 22:30:12 +0700

    Add files via upload

---
+++ Day1/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tugas 1 PHP Gits.id</title>
+
+  <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+
+  <link rel="stylesheet" href="style.css" />
+
+  <!-- font google -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="menu-1">
+    <section class="home" id="home">
+      <div class="home-content">
+        <h3><?php echo "Halo Namaku .." ?></h3>
+        <h1><?php echo "RizkyRachman" ?></h1>
+        <h3><?php echo "Salam Kenal Semua 😁" ?></h3>
+        <p>
+          <?php echo "Mahasiswa S1-Informatika Universitas Amikom Yogyakarta. Aktif dalam kegiatan organisasi, kepanitiaan, dan
+          asistensi. Terampil dalam berkomunikasi, koordinasi dalam beberapa kegiatan kepanitiaan dan asistensi di
+          lingkungan kampus. Sangat antusias untuk mempelajari hal-hal yang berkaitan dengan dunia programming,
+          khususnya dalam bidang Web-Development." ?>
+        </p>
+        <a href="#" class="btn">Contact Me</a>
+      </div>
+
+      <div class="home-img">
+        <img src="aset/Profile.jpeg" alt="" />
+      </div>
+    </section>
+  </div>
+
+</body>
+
+</html>
